c10bdb2d897b74f1d576266082b18e1242ee56c2
Ikmal Azman <opikmalazman@gmail.com>
2023-04-13 17:28:21 +0100

    Initial Commit

---
+++ CustomNavigationBar-SwiftUI/CustomNavigationBar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationBar_SwiftUIApp.swift
+//  CustomNavigationBar-SwiftUI
+//
+//  Created by Ikmal Azman on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationBar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
